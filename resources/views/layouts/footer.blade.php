18a1f4b7161769a540410469841999522f39434f
Desry Hernalin <desryhernalin@gmail.com>
2018-07-26 17:35:06 +0800

    Fixed layout and footer blades

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -16,12 +16,6 @@
       </div>
       <div class="col l3 s12">
         <h5 class="white-text">Connect</h5>
-        <!--<ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
-        </ul>-->
         <a href="https://www.facebook.com/InfiniteDATA2020/" class="waves-effect waves-light btn-floating btn-large social facebook"><i class="fa fa-facebook"></i> Sign in with facebook</a>
         <a href="https://www.linkedin.com/company/infinitedata/" class="waves-effect waves-light btn-floating btn-large social linkedin"><i class="fa fa-linkedin"></i> Sign in with linkedin</a>
       </div>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,16 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-    <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-    <!--Import materialize social -->
     <link rel="stylesheet" href="css/materialize-social.css">
-    <!--Import fontawesome.css-->
     <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.css">
 
-    <!--Let browser know website is optimized for mobile-->
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>InfiniteDATA Intranet</title>
@@ -23,28 +18,34 @@
     <div id="container">
        @yield('content')
     </div>
-@include('layouts.footer')
+
+    @include('layouts.footer')
     
 
-    <!--  Scripts-->
 <script type="text/javascript" src="js/jquery2-1-1.js"></script>
 <script type="text/javascript" src="js/app2.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
 <script src="js/init.js"></script>
 <script src='js/moment.min.js'></script>
-<!-- <script src='../lib/jquery.min.js'></script> -->
 <script src='js/fullcalendar.min.js'></script>
 
 
-    <script>
-        $(document).ready(function(){
-            $('.collapsible').collapsible();
-        });
-    </script>
-    <script>
-        $(document).ready(function(){
-            $('.parallax').parallax();
-        });
-    </script>
+<script>
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+    });
+</script>
+<script>
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
+</script>
+<script>
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+</script>
+
 </body>
 </html>
